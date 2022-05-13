775069b636f59916ff9cf2890106ae535d03a891
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2022-05-13 19:50:33 +0200

    sts + sn predic

--- variance.py
+++ variance.py
@@ -8,6 +8,7 @@ Created on Tue May 10 12:23:05 2022
 
 # Standard library imports:
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 # Local imports:
@@ -20,7 +21,8 @@ from post_selection import set_ctrl_vals_for_ps, post_select, momentum_select
 USE_ATOM_NUMBER_CALIB_UJ = True
 CTR_VAL_SHIFT = None  # If not calib: int/float; else: None
 SAVE_VARIANCE_FIG = False
-REL_FLUCT_TARGETS = [0.7, 5.7, 15.7]
+REL_FLUCT_TARGETS = [1, 3, 5]#[0.7, 5.7, 15.7]
+ETA = 0.53
 
 #%% Calculate and plot variance:
     
@@ -28,11 +30,14 @@ def plot_variance(REL_FLUCT_TARGETS):
     
     # Prepare figure:
     fig, ax1 = plt.subplots(figsize=(16, 9))
-    plot_colors = get_plot_colors('qualitative', 3, name='Set1')
-    lines, labels = [], []
+    plot_colors = get_plot_colors(
+        'qualitative',
+        len(REL_FLUCT_TARGETS),
+        name='Set1'
+        )
     
     for idx, rel_fluct_target in enumerate(REL_FLUCT_TARGETS):
-        
+                
         # Post select for each target value of relative fluctuations:
         cutoff_for_rel_target, ps_atom_numbers, ps_distr = post_select(
             rel_fluct_target,
@@ -46,42 +51,37 @@ def plot_variance(REL_FLUCT_TARGETS):
                                     ps_atom_numbers,
                                     ps_distr
                                     )
-        # Calculate variance and normalize:
-        variance = {
-            uj: mom_ps_atom_numbers[uj].var()
-            for uj in uj_vals
-            }
-        relative_fluctuations = {
-            uj: variance[uj] / ps_atom_numbers[uj].mean()
-            for uj in uj_vals
-            }
-        relative_fluctuations_error = {
-            uj: variance[uj] * np.sqrt(
-                2 / (mom_ps_atom_numbers[uj].count() - 1)
-                ) / ps_atom_numbers[uj].mean()
-            for uj in uj_vals
-            }
+        # Calculate variance and normalize with detection efficiency:
+        variance = mom_ps_atom_numbers.var()
+        relative_fluctuations = variance.divide(5e3) / (ETA**2)
+        relative_fluctuations_error = variance.mul(
+                                    np.sqrt(
+                                        2 / (mom_ps_atom_numbers.count() - 1)
+                                        )).divide(5e3)
+        
+        # Calculate relative fluctuations of post-selected shots:
+        fluct_std_perc = 100 * ps_atom_numbers.std().divide(
+                                                        ps_atom_numbers.mean()
+                                                        ).mean()
+        
+        # Predictions for other fluctuations:
+        sts = (mom_ps_atom_numbers.mean().pow(2)
+                * (fluct_std_perc / 100)**2 / (ETA**2) / 5e3)            
+       
+        shot_noise_norm =  mom_ps_atom_numbers.mean() / ETA / 5e3
+        
+        sts += shot_noise_norm
         
         # Plot:
-        relative_fluctuations_plot = [
-            relative_fluctuations[uj]
-            for uj in sorted(uj_vals)
-            ]
-        relative_fluctuations_error_plot = [
-            relative_fluctuations_error[uj]
-            for uj in sorted(uj_vals)
-            ]
-        fluct_std_perc = 100 * np.mean([
-            ps_atom_numbers[uj].std() / ps_atom_numbers[uj].mean()
-            for uj in sorted(uj_vals)
-            ])
         plot_label_rf = (r'$\frac{\Delta N}{N} = $'
                          + f'{fluct_std_perc:.2f}%  - Rel. Fluct.')
-        ylabel = r'$(\Delta N_{0}^{\mathrm{MCP}})^2\ /\ N^{\mathrm{MCP}}$'
+        plot_label_sts = (r'$\frac{\Delta N}{N} = $'
+                         + f'{fluct_std_perc:.2f}%  - Shot-to-shot + Shot-noise Fluct.')
+        ylabel = r'$\Delta N_{0}^2\ /\ N$'
         ax1.errorbar(
-            sorted(uj_vals),
-            relative_fluctuations_plot,
-            yerr=relative_fluctuations_error_plot,
+            uj_vals,
+            relative_fluctuations,
+            yerr=relative_fluctuations_error,
             color=plot_colors[idx],
             marker='o',
             markersize=5,
@@ -89,15 +89,20 @@ def plot_variance(REL_FLUCT_TARGETS):
             elinewidth=1.5,
             capsize=4.0,
             label=plot_label_rf,
-            zorder=idx
+            zorder=1/(idx+1),
             )
-        lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
-        lines.append([sum(lol, []) for lol in zip(*lines_labels)][0])
-        labels.append([sum(lol, []) for lol in zip(*lines_labels)][1])
+        ax1.plot(
+            uj_vals,
+            sts,
+            color=plot_colors[idx],
+            linestyle='--',
+            label=plot_label_sts
+            )
     ax1.grid()
     ax1.set_xlabel('Lattice depth [U/J]')
     ax1.set_ylabel(ylabel)
     plt.title('Relative atom number fluctuations in the BEC')        
+    lines, labels = [ax.get_legend_handles_labels() for ax in fig.axes][0]
     fig.legend(
         lines,
         labels,
@@ -106,7 +111,8 @@ def plot_variance(REL_FLUCT_TARGETS):
         )
     plt.tight_layout()
     plt.show()
-        
+  
+plot_variance(REL_FLUCT_TARGETS)      
 #%% Execution:
 
 if __name__ == '__main__':
@@ -129,18 +135,9 @@ if __name__ == '__main__':
 
 
 
+
     
-    # def condensed_fraction_scaling(uj):
-    
-    #     if uj > 25:
-    #         return 0
-    #     else:
-    #         return (1 - uj/26)**0.6
-    
-    # condensed_fraction = [condensed_fraction_scaling(uj) for uj in sorted(uj_vals)]
-    
-    # shot_noise = [condensed_fraction[idx] * 0.53 * 5e3 for idx, _ in enumerate(uj_vals)]
-    # shot_noise_norm = [shot_noise[idx] / 5e3 for idx, _ in enumerate(shot_noise)]
+  
     
     
     
