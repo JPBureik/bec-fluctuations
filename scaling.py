25de0c2aab1c1e350edceec480aae13c81dc7475
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2022-05-17 18:20:03 +0200

    delta n squared sur n_0 squared

--- scaling.py
+++ scaling.py
@@ -21,7 +21,7 @@ from variance import variance_norm, plot_variance
 
 """ ---------- INPUT ---------- """
 USE_ATOM_NUMBER_CALIB_UJ = False
-REL_FLUCT_TARGETS = [20]#, 6.1, 8, 15]
+REL_FLUCT_TARGETS = [5, 20]#, 6.1, 8, 15]
 ETA = 0.53
 UJ_SCALING = 24
 
