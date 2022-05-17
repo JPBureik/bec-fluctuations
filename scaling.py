9ef6eb3eabfaaef2f2e270e7c3e4b4b3e76c9c8f
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2022-05-17 17:02:59 +0200

    delta n squared sur n_0 squared

--- scaling.py
+++ scaling.py
@@ -98,11 +98,11 @@ for uj in [24,]:
              plot_ps=False
              )
              
-        if relative_fluctuations_error.loc[UJ_SCALING].max() > 3:#0.25:
+        if relative_fluctuations_error.loc[UJ_SCALING].max() > 0.25:
             relative_fluctuations.at[UJ_SCALING] = np.nan
             relative_fluctuations_error.at[UJ_SCALING] = np.nan
             
-        if relative_fluctuations.loc[UJ_SCALING].min() < 0.001:#0.1:
+        if relative_fluctuations.loc[UJ_SCALING].min() < 0.1:
             relative_fluctuations.at[UJ_SCALING] = np.nan
             relative_fluctuations_error.at[UJ_SCALING] = np.nan        
              
